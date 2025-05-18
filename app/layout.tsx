7523dd0d7fd3652b04e0d0ea6ebe4227ad8b7e05
Aravindh Bose <araviadm29@gmail.com>
2025-05-18 17:23:00 +0530

    First commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,10 @@ import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
+import Image from "next/image"
+// import {Features} from "@/public/features.png"
 import "./globals.css";
+import { features } from "process";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -39,39 +42,30 @@ export default function RootLayout({
         >
           <main className="min-h-screen flex flex-col items-center">
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+              <nav className="w-full flex justify-start border-b border-b-foreground/10 h-28">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
-                    </div>
-                  </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+                     <Image src = "/logo.png" height = {40} width = {160} alt ="logo"/>
                 </div>
               </nav>
               <div className="flex flex-col gap-20 max-w-5xl p-5">
                 {children}
               </div>
-
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
-                  Powered by{" "}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    Supabase
-                  </a>
-                </p>
-                <ThemeSwitcher />
+              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-4">
+                  <Image src="/features.png"  height = {160} width = {120} alt="Features" />
               </footer>
             </div>
           </main>
         </ThemeProvider>
       </body>
     </html>
+    // <html data-theme = 'white' className={geistSans.className} suppressHydrationWarning>
+    //   <body className="bg-background text-foreground">
+    //     <div>
+    //       <ThemeProvider>
+    //          {children}
+    //       </ThemeProvider>
+    //     </div>
+    //   </body>
+    // </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,127 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+ "use client";
+import { useState } from "react";
+import { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from "@/components/ui/domain-input-card"
+import {Button} from "@/components/ui/button";
+import dayjs from "dayjs";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
-export default async function Home() {
+
+
+
+export default function Home() {
+  const [orgName, setOrgName] = useState("");
+  const [outputFlag, setOutputFlag] = useState(false);
+  const [registration, setRegistration] = useState("");
+  const [expiration, setExpiration] = useState("");
+  const [hostedOn,setHostedOn] = useState("");
+  const [imgUrl, setImgUrl] = useState("");
+
+  const handleRevert =  (e:React.MouseEvent<HTMLButtonElement>) => {
+    setOutputFlag(false);
+    setOrgName("");
+
+  }
+ 
+  const handleSubmit = async (e:React.MouseEvent<HTMLButtonElement>) => {
+        try {
+            e.preventDefault();
+            const res =  await fetch(`https://rdap.verisign.com/com/v1/domain/${orgName}`)
+            const data = await res.json()
+
+            setRegistration(dayjs(data?.['events']?.[0]?.['eventDate'])?.format("MM/DD/YYYY"));
+            setExpiration(dayjs(data?.['events']?.[1]?.['eventDate'])?.format("MM/DD/YYYY"));
+            setHostedOn(data?.['entities']?.[0]?.['vcardArray']?.[1][1][3]);
+            setImgUrl(`https://logo.uplead.com/${orgName}`);
+            
+
+            console.log(data);
+            console.log(registration);
+            console.log(expiration);
+            console.log(hostedOn);
+
+            setOutputFlag(true)  ; 
+            // to display the data in UI
+
+
+            // if (data.events) {
+            //     data.events.map((item:any) => {
+            //         if (item.eventAction === "registration") {
+            //             setRegistration(new Date(item.eventDate).toLocaleString())
+            //         }
+            //         if (item.eventAction === "expiration") {
+            //             setExpire(new Date(item.eventDate).toLocaleString())
+            //         }
+            //     })
+            // }
+            // console.log(registration);
+            // console.log(expire);
+           
+        } catch(err) {
+            console.log(err);
+        }
+       
+  }
   return (
-    <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
-    </>
-  );
+    
+   (!outputFlag ? (
+   <Card>
+        <CardHeader>
+            <CardTitle>Domain Details Fetcher</CardTitle>
+            <CardDescription>Retrieves Critical Domain Information</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <input type="text" placeholder = "Enter the URL visible to public"onChange={e => setOrgName(e.target.value)} value={orgName}/>
+        </CardContent>
+        <CardFooter>
+            <Button onClick={handleSubmit}>Submit</Button>
+        </CardFooter>
+   </Card>
+
+  ) :
+   (
+   <Card>
+        <CardHeader>
+            <CardTitle>Domain Details</CardTitle>
+            <CardDescription>
+              <img alt="domain logo"
+                     height= "60pt"
+                     src= {imgUrl}
+                     width="60pt"/>
+                <p>{orgName}</p>
+            </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableBody>
+                <TableRow>
+                <TableCell className="font-medium">Registration Date</TableCell>
+                <TableCell>{registration}</TableCell>
+                </TableRow>
+                <TableRow>
+                <TableCell className="font-medium">Expiration Date</TableCell>
+                <TableCell>{expiration}</TableCell>
+                </TableRow>
+                <TableRow>
+                <TableCell className="font-medium">Hosted On</TableCell>
+                <TableCell>{hostedOn}</TableCell>
+                </TableRow>
+            </TableBody>
+           </Table>
+        </CardContent>
+        <CardFooter>
+            <Button onClick={handleRevert}>Revert</Button>
+        </CardFooter>
+   </Card>
+
+  )
+ )
+)
 }
